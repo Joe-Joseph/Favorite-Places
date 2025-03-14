6bb512843af33e8d4647f1dc4351df0a5a819c82
Joseph <jostheblessing@gmail.com>
2025-03-14 19:33:16 +0200

    Initial commit

---
+++ lib/screens/add_place.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shopping_list/providers/user_places.dart';
+import 'package:shopping_list/widgets/image_input.dart';
+
+class AddPlaceScreen extends ConsumerStatefulWidget {
+  const AddPlaceScreen({super.key});
+
+  @override
+  ConsumerState<AddPlaceScreen> createState() {
+    return _AddPlaceScreenState();
+  }
+}
+
+class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
+  final _titleController = TextEditingController();
+
+  void _savePlace() {
+    final _enteredTitle = _titleController.text;
+
+    if (_enteredTitle.isEmpty) {
+      return;
+    }
+
+    ref.read(userPlacesProvider.notifier).addPlace(_enteredTitle);
+    Navigator.of(context).pop();
+  }
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Add new Place"),
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(12),
+        child: Column(
+          children: [
+            TextField(
+              decoration: const InputDecoration(labelText: 'Title'),
+              controller: _titleController,
+              style:
+                  TextStyle(color: Theme.of(context).colorScheme.onBackground),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            ImageInput(),
+            const SizedBox(
+              height: 16,
+            ),
+            ElevatedButton(
+                onPressed: _savePlace, child: const Text("Add Place"))
+          ],
+        ),
+      ),
+    );
+  }
+}
